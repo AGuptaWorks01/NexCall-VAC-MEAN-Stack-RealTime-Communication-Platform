4d5c1cb27c0be6e6d1aea52ef9168417cafc1818
Anurag G <anurag.g@cnergee.com>
2025-06-18 17:21:10 +0530

    .

--- NextCall_api/src/congif/google.config.js
+++ NextCall_api/src/congif/google.config.js
@@ -7,4 +7,20 @@ const googleConfig = {
     scope: [ 'profile', 'email' ]
 };
 
+
 module.exports = googleConfig;
+
+
+
+
+// ==================== not in use ==========================
+
+// MONGO_URI = mongodb://localhost:27017/NextCall_VideoAudio
+// PORT = 3000
+// JWT_SECRET = I_am_JWT_SECRET
+
+// GOOGLE_CLIENT_ID = 830012090791 - f5t6mq6qi29p9lqead2euotha2a49fis.apps.googleusercontent.com
+// GOOGLE_CLIENT_SECRET = GOCSPX - XSzkI - CPHE9mlDYuyBdNne3g7_PW
+// REDIRECT_URL = http://localhost:3000/api/auth/google/callback
+// SESSION_SECRET = e25c48848a2dfd3626d81ec9363cc510bb8ea81562f02d6275c2f6dc45da7624
+
